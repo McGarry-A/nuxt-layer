7840a4ab5b2238a3a7b6363a039727b6b9dbd229
Ahmed McGarry <130065744+AhmedMcgarryUZ@users.noreply.github.com>
2023-12-26 19:19:48 +0000

    feat/product-section

---
+++ components/ProductSection/ProductSection.vue
@@ -0,0 +1,33 @@
+<template>
+    <section-wrapper class="flex flex-col text-center">
+        <template #title>Shop Now!</template>
+        <template #description>Check out or store of products!</template>
+        <splide
+            :options="options"
+            class="w-full cursor-grab"
+        >
+            <splide-slide
+                v-for="item in 10"
+                :key="item"
+            >
+                <card-product />
+            </splide-slide>
+        </splide>
+    </section-wrapper>
+</template>
+
+<script setup lang="ts">
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import '@splidejs/vue-splide/css';
+
+
+const options = {
+    type: 'loop',
+    padding: '5rem',
+    arrows: false,
+    pagination: false,
+    perPage: 4,
+    gap: '2rem',
+    autoplay: true,
+}
+</script>
